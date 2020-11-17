cb22f284df95834f05b9a0548b4888a3745781df
DESKTOP-2KIND8H\Raphael Tuksa <rapha.tuksa@hotmail.com>
2020-11-18 00:10:53 +0100

    Finnaly

--- src/pgdp/math/ThreeAndSeven.java
+++ src/pgdp/math/ThreeAndSeven.java
@@ -5,5 +5,20 @@ import static pgdp.MiniJava.*;
 public class ThreeAndSeven {
 	public static void main(String[] args) {
 
+		int n = readInt("Bitte Zahl eingeben:");
+		int sum = 0;
+
+		if (n >= 0) {
+			while (n > 2) {
+				if (n % 3 == 0 || n % 7 == 0) {
+					sum += n;
+				}
+				n--;
+			}
+			write(sum);
+		}
+		else
+			write("Fehler: n>=0 erwartet!");
+
 	}
 }
